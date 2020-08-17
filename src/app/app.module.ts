21698a81c7035eacb0e2b173718fda9bcddeec7b
ThrinadhAkkivarapu <thrinadh.akkivarapu@gmail.com>
2020-08-17 18:33:31 +0530

    Added duplicate array component
    Added duplicate array component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DuplicateComponent } from './duplicate/duplicate.component';
 import { PollingComponent } from './polling/polling.component';
 import { ThemeComponent } from './theme/theme.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: 'duplicate', component: DuplicateComponent },
+   { path: 'polling', component: PollingComponent},
+   { path: 'theme', component: ThemeComponent },
+   { path: '', pathMatch: 'full', redirectTo: 'duplicate' },
+      { path: '**', redirectTo: 'duplicate' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DuplicateComponent,
     PollingComponent,
-    ThemeComponent
+    ThemeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/duplicate/duplicate.component.ts
+++ src/app/duplicate/duplicate.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./duplicate.component.css']
 })
 export class DuplicateComponent implements OnInit {
-
+  duplicateArray = [10,11,8,9,12,23,11,8,7,10,12];
+  usingForEach: any;
+  usingReduce: any;
+  usingForOf: any;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    this.usingForEach = this.removeusingForEach(this.duplicateArray);
+    this.usingForOf = this.removeusingForOf(this.duplicateArray)
+    this.usingReduce = this.removeusingReduce(this.duplicateArray);  
   }
 
+  removeusingForEach(Array){
+    let emptyArray = [];
+    Array.forEach(element => {
+      if(!emptyArray.includes(element)){
+        emptyArray.push(element);
+      }
+    });
+    return emptyArray;    
+  }
+
+  removeusingForOf(array){
+    var emptyArray = [];
+    for(var value of array){
+        if(emptyArray.indexOf(value) === -1){
+          emptyArray.push(value);
+        }
+    }
+    return emptyArray;
+}
+
+  removeusingReduce(Array){
+    // let emptyArray = Array.reduce((a,b) =>{
+    //   if(a.indexOf(b) < 0) a.push(b);
+    //   return a;
+    // }, []);
+    // return emptyArray;
+    return Array.reduce((a,b) => a.includes(b) ? a : [...a, b], []);
+  }
 }
